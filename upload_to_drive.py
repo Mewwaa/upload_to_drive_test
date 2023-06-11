cd71c8a5ec649070fdfc47fb4ab405f595f58596
Ewa Zalewska <79149134+Mewwaa@users.noreply.github.com>
2023-06-11 19:42:38 +0200

    Update upload_to_drive.py

--- upload_to_drive.py
+++ upload_to_drive.py
@@ -4,25 +4,21 @@ from googleapiclient.discovery import build
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 
-def upload_file(file_path, 10wd3StRU5zWgARvINrG9Amu09h9L_AhD):
+def upload_file(file_path, folder_id):
     creds = None
 
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
     if os.path.exists('token.pickle'):
         with open('token.pickle', 'rb') as token:
             creds = pickle.load(token)
 
-    # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                '.github/workflows/client_secret_152312650973-s0vra8sl1uh2b19gvkickc26nm2ng9lg.apps.googleusercontent.com.json', ['https://www.googleapis.com/auth/drive'])
+                '.github/workflows/client_secret_152312650973-s0vra8sl1uh2b19gvkickc26nm2ng9lg.apps.googleusercontent.com.json',
+                ['https://www.googleapis.com/auth/drive'])
             creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
@@ -32,7 +28,7 @@ def upload_file(file_path, 10wd3StRU5zWgARvINrG9Amu09h9L_AhD):
 
     file_metadata = {
         'name': file_name,
-        'parents': [10wd3StRU5zWgARvINrG9Amu09h9L_AhD]
+        'parents': [folder_id]
     }
 
     media = MediaFileUpload(file_path, resumable=True)
