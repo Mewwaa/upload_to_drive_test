158129043138c9feab1848003cb7a3519df14b99
Ewa Zalewska <79149134+Mewwaa@users.noreply.github.com>
2023-06-11 20:14:35 +0200

    Update upload_to_drive.py

--- upload_to_drive.py
+++ upload_to_drive.py
@@ -45,6 +45,6 @@ def upload_files(directory_path, folder_id):
             print(f'Uploaded: {file_name} (File ID: {file.get("id")})')
 
 if __name__ == '__main__':
-    directory_path = 'files'
+    directory_path = ''
     folder_id = '10wd3StRU5zWgARvINrG9Amu09h9L_AhD'
     upload_files(directory_path, folder_id)
