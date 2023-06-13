0ec0d4169fd5cdca7c22a2f59e851e68513190d3
Ewa Zalewska <79149134+Mewwaa@users.noreply.github.com>
2023-06-13 14:59:49 +0200

    Update upload_to_drive.py

--- upload_to_drive.py
+++ upload_to_drive.py
@@ -1,54 +1,54 @@
-# import os
-# import pickle
-# from googleapiclient.discovery import build
-# from google.auth.transport.requests import Request
-# from google.oauth2.credentials import Credentials
-# from googleapiclient.http import MediaFileUpload
-# from google_auth_oauthlib.flow import InstalledAppFlow
+import os
+import pickle
+from googleapiclient.discovery import build
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from googleapiclient.http import MediaFileUpload
+from google_auth_oauthlib.flow import InstalledAppFlow
 
-# def upload_files(directory_path, folder_id):
-#     creds = None
+def upload_files(directory_path, folder_id):
+    creds = None
 
-#     if os.path.exists('token.pickle'):
-#         with open('token.pickle', 'rb') as token:
-#             creds = pickle.load(token)
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
 
-#     if not creds or not creds.valid:
-#         if creds and creds.expired and creds.refresh_token:
-#             creds.refresh(Request())
-#         else:
-#             flow = InstalledAppFlow.from_client_secrets_file(
-#                 '.github/workflows/client_secret_152312650973-s0vra8sl1uh2b19gvkickc26nm2ng9lg.apps.googleusercontent.com.json',
-#                 ['https://www.googleapis.com/auth/drive'])
-#             creds = flow.run_local_server(port=0)
-#         with open('token.pickle', 'wb') as token:
-#             pickle.dump(creds, token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                '.github/workflows/client_secret_152312650973-s0vra8sl1uh2b19gvkickc26nm2ng9lg.apps.googleusercontent.com.json',
+                ['https://www.googleapis.com/auth/drive'])
+            creds = flow.run_local_server(port=0)
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
 
-#     drive_service = build('drive', 'v3', credentials=creds)
+    drive_service = build('drive', 'v3', credentials=creds)
 
-#     print(f"Directory Path: {directory_path}")
+    print(f"Directory Path: {directory_path}")
 
-#     for file_name in os.listdir(directory_path):
-#         file_path = os.path.join(directory_path, file_name)
-#         print(f"Processing file: {file_path}")
+    for file_name in os.listdir(directory_path):
+        file_path = os.path.join(directory_path, file_name)
+        print(f"Processing file: {file_path}")
 
-#         if os.path.isfile(file_path) and (file_name.endswith('.pdf') or file_name.endswith('.txt')):
-#             file_metadata = {
-#                 'name': file_name,
-#                 'parents': [folder_id]
-#             }
+        if os.path.isfile(file_path) and (file_name.endswith('.pdf') or file_name.endswith('.txt')):
+            file_metadata = {
+                'name': file_name,
+                'parents': [folder_id]
+            }
 
-#             media = MediaFileUpload(file_path, resumable=True)
+            media = MediaFileUpload(file_path, resumable=True)
 
-#             file = drive_service.files().create(
-#                 body=file_metadata,
-#                 media_body=media,
-#                 fields='id'
-#             ).execute()
+            file = drive_service.files().create(
+                body=file_metadata,
+                media_body=media,
+                fields='id'
+            ).execute()
 
-#             print(f'Uploaded: {file_name} (File ID: {file.get("id")})')
+            print(f'Uploaded: {file_name} (File ID: {file.get("id")})')
 
-# if __name__ == '__main__':
-#     directory_path = ''
-#     folder_id = '10wd3StRU5zWgARvINrG9Amu09h9L_AhD'
-#     upload_files(directory_path, folder_id)
+if __name__ == '__main__':
+    directory_path = ''
+    folder_id = '10wd3StRU5zWgARvINrG9Amu09h9L_AhD'
+    upload_files(directory_path, folder_id)
