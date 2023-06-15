ce764a82f9a0ef9bc1210f93265c09d2d0d0266b
Ewa Zalewska <79149134+Mewwaa@users.noreply.github.com>
2023-06-15 21:13:41 +0200

    Update upload_to_drive.py

--- upload_to_drive.py
+++ upload_to_drive.py
@@ -1,45 +1,60 @@
-import os
 import requests
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
 
-gauth = GoogleAuth(settings_file='settings.yml')
-drive = GoogleDrive(gauth)
+url = "https://api.github.com/repos/Mewwaa/upload_to_drive_test/branches/main"
+response = requests.get(url)
 
-upload_folder = "/"  # Path to the folder you want to upload
-
-upload_file = []  # List of files to upload
-
-# Fetch all files from GitHub repository root
-repo_root_url = "https://api.github.com/repos/USERNAME/REPO_NAME/git/trees/main?recursive=1"
-response = requests.get(repo_root_url)
-data = response.json()
-
-# Make a request to the GitHub API
-response = requests.get("https://api.github.com/repos/Mewwaa/upload_to_drive_test/git/trees/main?recursive=1")
-
-# Check if the request was successful (status code 200)
 if response.status_code == 200:
-    # Print the JSON response
-    print(response.json())
+    # Request was successful
+    data = response.json()
+    commit_sha = data["commit"]["sha"]
+    print("Commit SHA:", commit_sha)
 else:
-    print("Request failed with status code:", response.status_code)
+    # Request failed
+    print("Error:", response.status_code)
 
 
-for item in data["tree"]:
-    if item["type"] == "blob" and (item["path"].endswith(".txt") or item["path"].endswith(".pdf")):
-        upload_file.append(item["path"])
+# import os
+# import requests
+# from pydrive.auth import GoogleAuth
+# from pydrive.drive import GoogleDrive
 
-# Upload files
-for file_path in upload_file:
-    file_url = f"https://raw.githubusercontent.com/Mewwaa/upload_to_drive_test/main/{file_path}"
-    r = requests.get(file_url, allow_redirects=True)
-    file_name = os.path.basename(file_path)
-    open(file_name, 'wb').write(r.content)
-    gfile = drive.CreateFile({'parents': [{"id": "10wd3StRU5zWgARvINrG9Amu09h9L_AhD"}]})
-    gfile.SetContentFile(file_name)
-    gfile.Upload()
-    os.remove(file_name)
+# gauth = GoogleAuth(settings_file='settings.yml')
+# drive = GoogleDrive(gauth)
+
+# upload_folder = "/"  # Path to the folder you want to upload
+
+# upload_file = []  # List of files to upload
+
+# # Fetch all files from GitHub repository root
+# repo_root_url = "https://api.github.com/repos/USERNAME/REPO_NAME/git/trees/main?recursive=1"
+# response = requests.get(repo_root_url)
+# data = response.json()
+
+# # Make a request to the GitHub API
+# response = requests.get("https://api.github.com/repos/Mewwaa/upload_to_drive_test/git/trees/main?recursive=1")
+
+# # Check if the request was successful (status code 200)
+# if response.status_code == 200:
+#     # Print the JSON response
+#     print(response.json())
+# else:
+#     print("Request failed with status code:", response.status_code)
+
+
+# for item in data["tree"]:
+#     if item["type"] == "blob" and (item["path"].endswith(".txt") or item["path"].endswith(".pdf")):
+#         upload_file.append(item["path"])
+
+# # Upload files
+# for file_path in upload_file:
+#     file_url = f"https://raw.githubusercontent.com/Mewwaa/upload_to_drive_test/main/{file_path}"
+#     r = requests.get(file_url, allow_redirects=True)
+#     file_name = os.path.basename(file_path)
+#     open(file_name, 'wb').write(r.content)
+#     gfile = drive.CreateFile({'parents': [{"id": "10wd3StRU5zWgARvINrG9Amu09h9L_AhD"}]})
+#     gfile.SetContentFile(file_name)
+#     gfile.Upload()
+#     os.remove(file_name)
 
 
 # import requests
