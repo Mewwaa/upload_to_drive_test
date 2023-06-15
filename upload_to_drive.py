ce0842961287ac9b4a84ea2b3955fba1729e2c71
Ewa Zalewska <79149134+Mewwaa@users.noreply.github.com>
2023-06-15 21:49:47 +0200

    Update upload_to_drive.py

--- upload_to_drive.py
+++ upload_to_drive.py
@@ -15,6 +15,7 @@ url = "https://api.github.com/repos/Mewwaa/upload_to_drive_test/branches/main"
 response = requests.get(url)
 data = response.json()
 tree_url = data["commit"]["tree"]["url"]
+print(tree_url)
 response_tree = requests.get(tree_url)
 
 
